77c5e93d4ff6088c9b758aa041fdd20efff46ed7
Tsuyoshi-Ishikawa <tsuyozo7@gmail.com>
2023-01-02 12:29:06 +0900

    enable to reply chatgpt message from API

--- functions/src/api/index.ts
+++ functions/src/api/index.ts
@@ -1,17 +1,56 @@
-import { UserStatementRepository } from '../repositories';
+import { AudioMessage } from '@line/bot-sdk';
+import { AudioDataRepository, ChatGptAnswerRepository } from '../repositories';
 import { getContent } from '../services/line';
-import { transcriptSpeech } from '../services';
+import { postChatGpt, transcriptSpeech, transcriptText } from '../services';
+import { getFileName } from '../utils';
+import { lineClient } from '../config';
 
-const userStatement = new UserStatementRepository();
+const chatGptAnswer = new ChatGptAnswerRepository();
+const audioDataService = new AudioDataRepository();
 
 export const post = async (params: { userId: string; messageId: string }) => {
   const { userId, messageId } = params;
 
   const content = await getContent(messageId);
   const userComment = await transcriptSpeech(content);
+  const chatGptStatementText = await postChatGpt(userComment);
+  const mp3Buffer = await transcriptText(userId, chatGptStatementText);
 
-  await userStatement.setStatement({
+  // line bot can only handle only m4a.
+  // After upload mp3, convert mp3 to m4a.
+  // https://developers.line.biz/en/reference/messaging-api/#audio-message
+  const transcriptMp3Url = getFileName({
+    prefix: 'answer',
     userId,
-    comment: userComment,
+    extension: 'mp3',
+  });
+  await audioDataService.uploadMP3(transcriptMp3Url, mp3Buffer);
+
+  const transcriptM4aFileName = getFileName({
+    prefix: 'answer',
+    userId,
+    extension: 'm4a',
+  });
+
+  await audioDataService.uploadM4aFromMP3File(
+    transcriptM4aFileName,
+    transcriptMp3Url,
+  );
+
+  const signedUrl = await audioDataService.getSignedM4aUrl(
+    transcriptM4aFileName,
+  );
+  const message = {
+    type: 'audio',
+    originalContentUrl: signedUrl,
+    duration: 120000, // Can transmit up to 2 minutes of audio data
+  } as AudioMessage;
+  await lineClient.pushMessage(userId, message);
+
+  await chatGptAnswer.setAnswer({
+    userId,
+    audioUrl: transcriptM4aFileName,
+    userStatementText: userComment,
+    chatGptStatementText,
   });
 };
